fe75e1b412eb41da6863c253c48f6195931b9452
webC2017 <you@example.com>
2017-02-15 17:16:05 +0800

    Initial commit

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by 2429916391@qq.com on 2017/2/15 17:14.
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("Hello-world");
+    }
+}
